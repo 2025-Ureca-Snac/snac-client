d263258ea3d9a8f99fb83db931ead9584a31ebbc
yshls <shluxnsal01@gmail.com>
2025-07-11 15:58:06 +0900

    feat: 카드 목록 API(api/cards) 연동

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
-import { Button } from './(shared)/components/Button';
+// import { Button } from './(shared)/components/Button';
 import { Header } from './(shared)/components/Header';
 import { Footer } from './(shared)/components/Footer';
+import { HomePageClient } from './(shared)/components/HomePageClient';
 
-export default function Home() {
+async function getItem() {
+  const res = await fetch('http://localhost:3000/api/cards', {
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+export default async function Home() {
+  const cards = await getItem();
   return (
     <>
       <Header />
       <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center">
-        홈화면 레이아웃 기초
-        <Button>테스트</Button>
+        {/* 홈화면 레이아웃 기초
+        <Button>테스트</Button> */}
+        <HomePageClient cards={cards} />
       </div>
+
       <Footer />
     </>
   );
