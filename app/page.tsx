49ed0758c2c6a714f011b9e972c3758aa938d7e7
hyonun321 <hyo_nun@kakao.com>
2025-07-14 21:31:54 +0900

    fix: 클라이언트에서 사용하지 않기때문에 환경변수 이름 변경
    - next를 붙이면 클라이언트 브라우저에서 조회가 가능하다

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Header } from './(shared)/components/Header';
 import { Footer } from './(shared)/components/Footer';
 import { HomePageClient } from './(shared)/components/HomePageClient';
 
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_BASE = process.env.API_BASE_URL;
 
 async function getCardData() {
   const res = await fetch(`${API_BASE}/api/cards`, {
