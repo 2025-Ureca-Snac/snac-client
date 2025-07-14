fd6d90820c1229fbe9c09f6a112bb2a4887bba6a
yshls <shluxnsal01@gmail.com>
2025-07-14 16:46:45 +0900

    feat: 틸 그린 색상 추가 (#38CB89)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,6 +67,7 @@ const config: Config = {
         'candy-pink': '#FF66C4',
         'packet-golden': '#D09436',
         'cloud-white': '#F3F3F3',
+        'teal-green': '#38CB89',
         gray: {
           50: '#FAFAFA',
           100: '#F5F5F5',
