b54aa27898a355b33a8d6d3d1379ef838d078232
hyonun321 <hyo_nun@kakao.com>
2025-07-15 16:02:58 +0900

    fix: react key값을 index가 아닌 url로 변경

--- app/blog/admin/page.tsx
+++ app/blog/admin/page.tsx
@@ -251,7 +251,7 @@ export default function BlogAdminPage() {
                   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     {formData.images.map((image, index) => (
                       <div
-                        key={index}
+                        key={image}
                         className="border border-gray-200 rounded-lg p-3"
                       >
                         <div className="relative mb-2">
