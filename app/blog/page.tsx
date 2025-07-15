a2c245693c09d9ff06f6721dc99e1e39fdfd6b05
hyonun321 <hyo_nun@kakao.com>
2025-07-15 15:10:47 +0900

    chore: 불필요한 postClick 함수 제거
    - 이제 모달로 보여지므로, router 이동할 필요가 없음

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,6 @@
 
 import { Footer } from '@/app/(shared)/components/Footer';
 import { Header } from '@/app/(shared)/components/Header';
-import { BlogPost } from '@/app/(shared)/components/BlogCard';
 import { BlogHero } from './components/BlogHero';
 import { BlogContent } from './components/BlogContent';
 import { BLOG_POSTS } from './data/blogPosts';
@@ -11,12 +10,6 @@ export default function BlogPage() {
   const handleShowMore = () => {
     console.log('Show more clicked!');
     // 여기에 더 많은 포스트를 로드하는 로직 추가
-  };
-
-  const handlePostClick = (post: BlogPost) => {
-    // console.log('Post clicked:', post.title);
-    // 개별 포스트 페이지로 이동
-    // router.push(`/blog/${post.id}`);
   };
 
   const handleSortChange = (sortBy: string) => {
@@ -31,7 +24,6 @@ export default function BlogPage() {
       <BlogContent
         posts={BLOG_POSTS}
         onShowMore={handleShowMore}
-        onPostClick={handlePostClick}
         onSortChange={handleSortChange}
       />
       <Footer />
