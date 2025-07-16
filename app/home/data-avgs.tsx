1da8162e9c67c74424cd1c95b54b4dba4cce96ea
yshls <shluxnsal01@gmail.com>
2025-07-16 17:47:12 +0900

    refactor: import 알파벳으로 정리

--- app/home/data-avgs.tsx
+++ app/home/data-avgs.tsx
@@ -1,7 +1,7 @@
 'use client';
+import React, { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
-import React, { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
 
 interface DataAvgProps {
   providers: string[];
