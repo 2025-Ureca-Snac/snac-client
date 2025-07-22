8508dcbcaf7f04538178748640e248e88fad1084
seungwoo <zoozoo1302@gmail.com>
2025-07-22 18:06:01 +0900

    docs: jsDoc 작성을 통하여 코드 가독성 향상

--- app/(shared)/types/modal-store.ts
+++ app/(shared)/types/modal-store.ts
@@ -1,6 +1,13 @@
 /**
  * @author 이승우
- * @description 모달 타입( search(검색), payment(결제), confirm(확인), alert(알림), blog(블로그) )
+ * @description 모달 타입
+ * @interface ModalType
+ * @property {string} search 검색
+ * @property {string} payment 결제
+ * @property {string} confirm 확인
+ * @property {string} alert 알림
+ * @property {string} blog 블로그
+ * @property {null} null 모달 타입 없음
  */
 export type ModalType =
   | 'search'
@@ -13,13 +20,14 @@ export type ModalType =
 /**
  * @author 이승우
  * @description 모달 상태
- * @param isOpen 모달 열림 여부
- * @param modalType 모달 타입
- * @param modalData 모달 데이터
+ * @interface ModalState
+ * @property {boolean} isOpen 모달 열림 여부
+ * @property {ModalType} modalType 모달 타입
+ * @property {Record<string, unknown>} modalData 모달 데이터
  *
- * @param openModal 모달 열기 액션
- * @param closeModal 모달 닫기 액션
- * @param setModalData 모달 데이터 설정 액션
+ * @property {Function} openModal 모달 열기 액션
+ * @property {Function} closeModal 모달 닫기 액션
+ * @property {Function} setModalData 모달 데이터 설정 액션
  */
 export interface ModalState {
   // 상태
--- app/(shared)/types/sign-up.ts
+++ app/(shared)/types/sign-up.ts
@@ -2,15 +2,16 @@
  * 회원가입 폼 데이터
  * @author 이승우
  * @description 회원가입 폼 데이터
- * @property name : 이름
- * @property nickname: 닉네임
- * @property email: 이메일
- * @property phoneNumber: 전화번호
- * @property birthDate: 생년월일
- * @property password: 비밀번호
- * @property passwordConfirm: 비밀번호 확인
- * @property emailVerificationCode: 이메일 인증코드
- * @property phoneVerificationCode: 전화번호 인증코드
+ * @interface SignUpFormData
+ * @property {string} name 이름
+ * @property {string} nickname 닉네임
+ * @property {string} email 이메일
+ * @property {string} phoneNumber 전화번호
+ * @property {Date} birthDate 생년월일
+ * @property {string} password 비밀번호
+ * @property {string} passwordConfirm 비밀번호 확인
+ * @property {string} emailVerificationCode 이메일 인증코드
+ * @property {string} phoneVerificationCode 전화번호 인증코드
  */
 export interface SignUpFormData {
   name: string;
@@ -27,8 +28,9 @@ export interface SignUpFormData {
 /**
  * @author 이승우
  * @description 비밀번호 일치 상태
- * @property none: 비밀번호 입력 안됨
- * @property match: 비밀번호 일치
- * @property mismatch: 비밀번호 불일치
+ * @interface PasswordMatchState
+ * @property {string} none 비밀번호 입력 안됨
+ * @property {string} match 비밀번호 일치
+ * @property {string} mismatch 비밀번호 불일치
  */
 export type PasswordMatchState = 'none' | 'match' | 'mismatch';
