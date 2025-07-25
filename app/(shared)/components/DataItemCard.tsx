6f945af322471ee7464df345ab95859c3903f3a6
yshls <shluxnsal01@gmail.com>
2025-07-25 13:25:07 +0900

    feat: 작성자일경우 구매, 판매 버튼 가리고, 뱃지로 작성자 표시
    -hover나 active일때 색상추가 구매: 검은색 -> 빨간색, 판매: 핑크색 -> 차콜색

--- app/(shared)/components/DataItemCard.tsx
+++ app/(shared)/components/DataItemCard.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Image from 'next/image';
 import { Button } from './Button';
 import { PriceUnit } from '@/app/(shared)/types';
+import { useAuthStore } from '@/app/(shared)/stores/auth-store';
 
 interface DataItemCardProps {
   imageUrl: string;
@@ -32,6 +33,8 @@ export const DataItemCard = ({
   buyButtonText,
   onClickBuy,
 }: DataItemCardProps) => {
+  const loggedInUser = useAuthStore((state) => state.user);
+  const isMyPost = loggedInUser === email;
   const displayPrice =
     unit === 'snack' ? (
       <span className="inline-flex items-center">
@@ -53,16 +56,20 @@ export const DataItemCard = ({
     buyButtonText ?? (isBuyView ? '판매하기' : '구매하기');
 
   const buttonColorClass = isBuyView
-    ? 'bg-candy-pink hover:bg-[#ff93c4]'
-    : 'bg-gray-900 hover:bg-gray-800';
+    ? 'bg-candy-pink hover:bg-gray-700 active:bg-gray-800'
+    : 'bg-gray-900 hover:bg-red active:bg-red';
   return (
     <div className="transition-transform duration-300 hover:-translate-y-[2px] hover:scale-[1.03] relative bg-[#F3F5F7] rounded-2xl shadow-md w-card-sm h-card-sm md:w-card-md md:h-card-md flex flex-col p-3">
       {isNew && (
-        <span className="absolute z-10 bg-red text-white text-regular-sm md:text-regular-md font-bold w-[57px] md:w-[67px] h-[24px]  rounded-[4px] flex items-center justify-center ">
+        <span className="absolute z-10 bg-red text-white text-regular-2xs md:text-regular-xs font-bold w-[47px]  md:w-[57px] h-[20px] md:h-[24px]  rounded-[16px] flex items-center justify-center ">
           {newBadgeText}
         </span>
       )}
-
+      {isMyPost && (
+        <span className="absolute top-3 right-3 z-10 bg-gray-400   text-white text-regular-2xs md:text-regular-xs  w-[47px]  md:w-[57px] h-[20px] md:h-[24px]  rounded-[16px] font-bold px-1 py-1 flex items-center justify-center ">
+          작성자
+        </span>
+      )}
       <div className="relative h-[64px] md:h-[125px] mt-[35px] md:mt-[80px]">
         <Image
           src={imageUrl}
@@ -82,14 +89,16 @@ export const DataItemCard = ({
       </div>
 
       <div className="flex justify-center ">
-        <Button
-          onClick={() => {
-            onClickBuy({ email, createdAt });
-          }}
-          className={`w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md ${buttonColorClass} transition text-regular-md border rounded-lg flex items-center justify-center`}
-        >
-          {finalButtonText}
-        </Button>
+        {!isMyPost && (
+          <Button
+            onClick={() => {
+              onClickBuy({ email, createdAt });
+            }}
+            className={`w-btn-sm h-btn-sm md:w-btn-md md:h-btn-md ${buttonColorClass} transition text-regular-md border rounded-lg flex items-center justify-center`}
+          >
+            {finalButtonText}
+          </Button>
+        )}
       </div>
     </div>
   );
