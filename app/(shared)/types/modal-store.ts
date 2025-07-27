3578ea9b726c45fc4b018b6ac42ca2a78ad202d6
seungwoo <zoozoo1302@gmail.com>
2025-07-27 15:09:34 +0900

    refactor: 가독성 향상

--- app/(shared)/types/modal-store.ts
+++ app/(shared)/types/modal-store.ts
@@ -1,6 +1,21 @@
 /**
  * @author 이승우
  * @description 모달 타입
+ * @interface ModalType
+ * @property {string} search 검색 모달
+ * @property {string} payment 결제 모달
+ * @property {string} confirm 확인 모달
+ * @property {string} alert 알림 모달
+ * @property {string} blog 블로그 모달
+ * @property {string} favorite-list 즐겨찾기 모달
+ * @property {string} change-password 비밀번호 변경 모달
+ * @property {string} change-phone 전화번호 변경 모달
+ * @property {string} change-nickname 닉네임 변경 모달
+ * @property {string} social-login 소셜 로그인 모달
+ * @property {string} service-guide 서비스 가이드 모달
+ * @property {string} privacy-policy 개인정보 처리방침 모달
+ * @property {string} theme 테마 모달
+ * @property {null} null 모달 없음
  */
 export type ModalType =
   | 'search'
