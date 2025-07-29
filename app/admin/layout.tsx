ea381ad3df7b31fba2b3a887994a6429cebb77ad
yshls <shluxnsal01@gmail.com>
2025-07-29 16:14:06 +0900

    fix: 사이드바 높이 올바르게 설정

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -37,7 +37,7 @@ function Sidebar() {
 
   return (
     <aside
-      className={`fixed md:relative z-30 flex-shrink-0 w-64 h-[full] text-white transition-transform duration-300 ease-in-out ${
+      className={`fixed md:relative z-30 flex-shrink-0 w-64 h-full text-white transition-transform duration-300 ease-in-out ${
         isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
       } md:translate-x-0`}
     >
