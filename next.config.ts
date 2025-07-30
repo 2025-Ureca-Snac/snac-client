a30ed771c39986a976448e85fa1634bcabd07870
yshls <shluxnsal01@gmail.com>
2025-07-30 13:08:25 +0900

    feat: next.config에 S3 이미지 및 SVG 컴포넌트 import 기능 추가
    + 충돌 해결

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,23 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'snac-static.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
-
     return config;
   },
 };
