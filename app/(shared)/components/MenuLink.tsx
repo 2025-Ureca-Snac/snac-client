da5eb48680bf6913f9d0b8db3c48881c3cb20056
yshls <shluxnsal01@gmail.com>
2025-08-02 00:07:45 +0900

    feat: aria-label 추가

--- app/(shared)/components/MenuLink.tsx
+++ app/(shared)/components/MenuLink.tsx
@@ -7,13 +7,13 @@ interface MenuLinkProps {
   alt: string;
   text: string;
 }
-
 export const MenuLink = ({ href, IconComponent, alt, text }: MenuLinkProps) => (
   <Link
     href={href}
     className="flex items-center gap-2 transition-opacity hover:opacity-50"
+    aria-label={alt}
   >
-    <IconComponent width={24} height={24} alt={alt} />
+    <IconComponent width={24} height={24} aria-hidden="true" />
     <span className="hidden md:flex text-midnight-black dark:text-white text-regular-sm">
       {text}
     </span>
