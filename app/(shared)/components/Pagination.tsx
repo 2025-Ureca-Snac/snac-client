01613b2244676f8d1ae7ece993898b7cb9748b41
yshls <shluxnsal01@gmail.com>
2025-08-02 12:57:11 +0900

    refactor: 중복된 코드 제거

--- app/(shared)/components/Pagination.tsx
+++ app/(shared)/components/Pagination.tsx
@@ -74,8 +74,7 @@ export const Pagination = ({
   const defaultButtonStyles =
     'bg-white border-gray-300 text-gray-700 hover:bg-gray-100 dark:bg-midnight-black dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-800';
 
-  const activeButtonStyles =
-    'bg-gray-700 border-gray-700 text-white dark:bg-gray-700 dark:border-gray-700 dark:text-white';
+  const activeButtonStyles = 'bg-gray-700 border-gray-700 text-white';
 
   const disabledButtonStyles = 'opacity-50 cursor-not-allowed';
 
