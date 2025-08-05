06eebcc3cd34690d70ad97ed75499a5319ba09f3
yshls <shluxnsal01@gmail.com>
2025-08-05 10:42:31 +0900

    feat: PlusIcon 컴포넌트를 사용하여 CreateButton의 아이콘 변경

--- app/(shared)/components/CreateButton.tsx
+++ app/(shared)/components/CreateButton.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import PlusIcon from '@/public/plus.svg';
 
 interface CreateButtonProps {
   onClick: () => void;
@@ -13,21 +14,7 @@ export function CreateButton({ onClick }: CreateButtonProps) {
       className="fixed bottom-6 right-6 z-50 flex items-center gap-2 rounded-full bg-gradient-to-br from-burst-lime to-[#38CB89] px-5 py-3 text-sm font-bold text-white shadow-lg transition-opacity duration-300 ease-in-out hover:brightness-95 focus:outline-none md:hidden"
       aria-label="거래 등록"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="3"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <line x1="12" y1="5" x2="12" y2="19"></line>
-        <line x1="5" y1="12" x2="19" y2="12"></line>
-      </svg>
-
+      <PlusIcon />
       <span>거래 등록</span>
     </button>
   );
