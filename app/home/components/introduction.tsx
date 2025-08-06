5ad53c4239f0999820254f92dedce4fc4599e423
yshls <shluxnsal01@gmail.com>
2025-08-06 16:00:56 +0900

    fix: 에러 수정

--- app/home/components/introduction.tsx
+++ app/home/components/introduction.tsx
@@ -4,7 +4,6 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
 import {
-  Lock,
   Newspaper,
   Zap,
   Clock,
